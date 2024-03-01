3284f26b57a611ff8e06ce14ea9a0dfa0b725019
Brandie Lucano <blucano.dev@gmail.com>
2024-03-01 12:03:36 -0800

    case insensitive and people sort working and tests pass.

--- sorting/people-sort/peopleSort.ts
+++ sorting/people-sort/peopleSort.ts
@@ -3,22 +3,23 @@ type tPerson = {fname: string, lname: string};
 /** Sort array of persons in place (lname > fname). Returns array. */
 
 function peopleSort(arr: tPerson[]): tPerson[] {
-  return arr.sort((a: tPerson, b: tPerson): number => {
-        if (a.lname < b.lname) {
-          return -1;
-        } else if (a.lname > b.lname) {
-          return +1;
-        } else {
-          if (a.fname < b.fname) {
-            return -1;
-          } else if (a.fname > b.fname) {
-            return +1;
-          } else {
-            return 0;
-          }
-        }
+  return arr.sort((a, b) => {
+    if(a.lname < b.lname){
+      return -1;
+    }
+    if(a.lname > b.lname){
+      return 1
+    }
+    if(a.lname === b.lname){
+      if(a.fname < b.fname){
+        return -1;
       }
-  );
+      if(a.fname > b.fname){
+        return 1
+      }
+    }
+    return 0;
+  })
 }
 
 export { peopleSort };
